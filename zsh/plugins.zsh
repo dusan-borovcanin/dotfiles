c945f3e675d39a99f5c0ca2af70a4df03bbce44d
Dusan Borovcanin <borovcanindusan1@gmail.com>
2018-12-13 12:42:41 +0100

    Remove ZSH Vim mode

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,6 +1,5 @@
 source $ZSH_CUSTOM/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $ZSH_CUSTOM/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source $ZSH_CUSTOM/plugins/zsh-vim-mode/zsh-vim-mode.zsh
 source <(kubectl completion zsh)
 
 #Autosuggestion color
