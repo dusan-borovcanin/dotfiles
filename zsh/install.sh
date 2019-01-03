25d9143d57e66fbc73e690c97ed704474b4154b5
Dusan Borovcanin <borovcanindusan1@gmail.com>
2019-01-03 21:21:17 +0100

    Automate .zshrc and .Xdefaults installation

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/dotfiles/}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/dotfiles/}/plugins/zsh-syntax-highlighting
-
+cp ../.Xdefaults ~
+cp ./.zshrc ~
