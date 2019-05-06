c0a7b371dc904fde407a5ef41b456edf0fe95a68
Dušan Borovčanin <dusan.borovcanin@mainflux.com>
2019-05-06 20:08:05 +0200

    Use sed to update config.ini

--- i3/polybar/polybar/scripts/type.sh
+++ i3/polybar/polybar/scripts/type.sh
@@ -5,40 +5,49 @@
 
 PDIR="$HOME/.config/polybar"
 LAUNCH="polybar-msg cmd restart"
- 
+START=155
+START_NEW=154
+END=215
+
 if  [[ $1 = "-type-1" ]]; then
 # Copying The File
-cp $PDIR/config-t1.ini $PDIR/config.ini
+sed -i "${START},${END}d" $PDIR/config.ini
+sed -i "${START_NEW}r $PDIR/config-t1.ini" $PDIR/config.ini
 # Restarting polybar
 $LAUNCH &
 
 elif  [[ $1 = "-type-2" ]]; then
 # Copying The File
-cp $PDIR/config-t2.ini $PDIR/config.ini
+sed -i "${START},${END}d" $PDIR/config.ini
+sed -i "${START_NEW}r $PDIR/config-t2.ini" $PDIR/config.ini
 # Restarting polybar
 $LAUNCH &
 
 elif  [[ $1 = "-type-3" ]]; then
 # Copying The File
-cp $PDIR/config-t3.ini $PDIR/config.ini
+sed -i "${START},${END}d" $PDIR/config.ini
+sed -i "${START_NEW}r $PDIR/config-t3.ini" $PDIR/config.ini
 # Restarting polybar
 $LAUNCH &
 
 elif  [[ $1 = "-type-4" ]]; then
 # Copying The File
-cp $PDIR/config-t4.ini $PDIR/config.ini
+sed -i "${START},${END}d" $PDIR/config.ini
+sed -i "${START_NEW}r $PDIR/config-t4.ini" $PDIR/config.ini
 # Restarting polybar
 $LAUNCH &
 
 elif  [[ $1 = "-type-5" ]]; then
 # Copying The File
-cp $PDIR/config-t5.ini $PDIR/config.ini
+sed -i "${START},${END}d" $PDIR/config.ini
+sed -i "${START_NEW}r $PDIR/config-t5.ini" $PDIR/config.ini
 # Restarting polybar
 $LAUNCH &
 
 elif  [[ $1 = "-type-6" ]]; then
 # Copying The File
-cp $PDIR/config-t6.ini $PDIR/config.ini
+sed -i "${START},${END}d" $PDIR/config.ini
+sed -i "${START_NEW}r $PDIR/config-t6.ini" $PDIR/config.ini
 # Restarting polybar
 $LAUNCH &
 
