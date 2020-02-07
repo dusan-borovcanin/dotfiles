95ee75e6f8ad9dca5f8d7b5c08c6740194dbe9c8
Dušan Borovčanin <dusan.borovcanin@mainflux.com>
2020-02-07 19:46:55 +0100

    Improve startup script

--- config/i3/startup.sh
+++ config/i3/startup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Turn built-in monitor off if an external monitor(s) is connected.
+count=$(xrandr | grep "DP-*. connected" | wc | awk '{print $1}')
+[ count > 1 ] && xrandr --output eDP-1 --off
+
 brave & \
 code & \
 slack &
