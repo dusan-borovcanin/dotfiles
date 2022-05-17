a405645a1562a02c196474ec2a15e8652d2e276a
Dusan Borovcanin <borovcanindusan1@gmail.com>
2022-05-17 09:49:43 +0200

    Add YouTube to quick search

--- go/search.go
+++ go/search.go
@@ -9,7 +9,11 @@ import (
 	"strings"
 )
 
-const browser = "brave"
+const (
+	browser   = "brave"
+	shell     = "bash"
+	shellArgs = "-c"
+)
 
 func main() {
 	if len(os.Args) != 2 {
@@ -22,25 +26,31 @@ func main() {
 	case strings.HasPrefix(a, "t "):
 		q := url.URL{Path: strings.TrimSpace(strings.TrimPrefix(a, "t "))}
 		p := q.EscapedPath()
-		c = exec.Command("bash", "-c", fmt.Sprintf("%s https://translate.google.rs/?sl=sr\\&tl=en\\&text=%s\\&op=translate", browser, p))
-	case strings.HasPrefix(a, "!t "):
+		c = exec.Command(shell, shellArgs, fmt.Sprintf("%s https://translate.google.rs/?sl=sr\\&tl=en\\&text=%s\\&op=translate", browser, p))
+	case strings.HasPrefix(a, "t! "):
 		q := url.URL{Path: strings.TrimSpace(strings.TrimPrefix(a, "!t "))}
 		p := q.EscapedPath()
-		c = exec.Command("bash", "-c", fmt.Sprintf("%s https://translate.google.rs/?sl=en\\&tl=sr\\&text=%s\\&op=translate", browser, p))
+		c = exec.Command(shell, shellArgs, fmt.Sprintf("%s https://translate.google.rs/?sl=en\\&tl=sr\\&text=%s\\&op=translate", browser, p))
+	case strings.HasPrefix(a, "yt "):
+		q := url.QueryEscape(strings.TrimPrefix(a, "yt "))
+		c = exec.Command(shell, shellArgs, fmt.Sprintf("%s https://www.youtube.com/results?search_query=%s", browser, q))
 	case a == "mondo":
-		c = exec.Command("bash", "-c", fmt.Sprintf("%s www.mondo.rs", browser))
+		c = exec.Command(shell, shellArgs, fmt.Sprintf("%s www.mondo.rs", browser))
 	case a == "fb":
-		c = exec.Command("bash", "-c", fmt.Sprintf("%s www.facebook.com", browser))
+		c = exec.Command(shell, shellArgs, fmt.Sprintf("%s www.facebook.com", browser))
 	case a == "wa":
-		c = exec.Command("bash", "-c", fmt.Sprintf("%s https://web.whatsapp.com", browser))
+		c = exec.Command(shell, shellArgs, fmt.Sprintf("%s https://web.whatsapp.com", browser))
 	case a == "mf":
-		c = exec.Command("bash", "-c", fmt.Sprintf("%s https://github.com/mainflux", browser))
+		c = exec.Command(shell, shellArgs, fmt.Sprintf("%s https://github.com/mainflux", browser))
 	default:
-		c = exec.Command("bash", "-c", fmt.Sprintf("%s https://www.google.com/search?q=%s", browser, url.QueryEscape(a)))
+		c = exec.Command(shell, shellArgs, fmt.Sprintf("%s https://www.google.com/search?q=%s", browser, url.QueryEscape(a)))
 	}
 
+	if c == nil {
+		log.Fatal("Commnad does not exist!")
+	}
 	if err := c.Run(); err != nil {
-		log.Fatalf("An error ocurred: %s", err)
+		log.Fatalf("An error ocurred: %s!", err)
 	}
 	os.Exit(0)
 }
