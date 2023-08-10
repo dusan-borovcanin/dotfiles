b8498816e73deef9c03e0680daac8bc805cb35d1
Dusan Borovcanin <borovcanindusan1@gmail.com>
2023-08-10 14:23:22 +0200

    Add config for new device
    Start cleanup process.
    
    Signed-off-by: Dusan Borovcanin <borovcanindusan1@gmail.com>

--- config/i3/scripts/search.sh
+++ config/i3/scripts/search.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-q=$(echo "" | dmenu -fn 'DejaVu Sans Mono Nerd Font:size=14' -nb '#004D40' -sb '#004D40' -i -p "Search web:")
+q=$(echo "" | dmenu -fn 'DejaVu Sans Mono Nerd Font:size=15' -nb '#282A2E' -sb '#dbc291' -i -p "Search web:")
 dotfiles_search "$q"
--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -4,7 +4,8 @@ source <(kubectl completion zsh)
 
 #Autosuggestion color
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=242'
-source ~/.fzf.zsh
+source /usr/share/fzf/key-bindings.zsh
+source /usr/share/fzf/completion.zsh
 #Theme settings
 export BULLETTRAIN_GO_PREFIX='\ue626'
 export BULLETTRAIN_GIT_COLORIZE_DIRTY=true
