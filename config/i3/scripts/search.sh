82403c3aaab4e5470103b1efa74ca81a99414cbb
Dusan Borovcanin <borovcanindusan1@gmail.com>
2023-08-15 15:14:52 +0200

    Fix search size
    Signed-off-by: Dusan Borovcanin <borovcanindusan1@gmail.com>

--- config/i3/scripts/search.sh
+++ config/i3/scripts/search.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-q=$(echo "" | dmenu -fn 'DejaVu Sans Mono Nerd Font:size=15' -nb '#282A2E' -sb '#282A2E' -i -p "Search web:")
+q=$(echo "" | dmenu -fn 'DejaVu Sans Mono Nerd Font:size=16' -nb '#282A2E' -sb '#282A2E' -i -p "Search web:")
 dotfiles_search "$q"
