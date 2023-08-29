93bd81a0e3557ca505f3ad7be9e6389f849bf0cd
Dusan Borovcanin <borovcanindusan1@gmail.com>
2023-08-29 08:37:33 +0200

    Fix startup script
    Signed-off-by: Dusan Borovcanin <borovcanindusan1@gmail.com>

--- config/i3/scripts/startup.sh
+++ config/i3/scripts/startup.sh
@@ -5,13 +5,14 @@
 
 # Turn built-in monitor off if an external monitor(s) is connected.	
 count=$(xrandr | grep "DP-*. connected" | wc | awk '{print $1}')	
-if [ "$count" -gt "1" ]; then	
-    xrandr --output eDP1 --off	
+if [ "$count" -gt "0" ]; then
+    xrandr --output eDP1 --off
 fi	
 
+# For Thinkpad.
 count=$(xrandr | grep "DisplayPort-*. connected" | wc | awk '{print $1}')	
-if [ "$count" -gt "1" ]; then	
-    xrandr --output eDP1 --off	
+if [ "$count" -gt "0" ]; then
+    xrandr --output eDP --off
 fi	
 
 day=$(date '+%a')	
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -16,9 +16,9 @@ source ~/dotfiles/zsh/plugins.zsh
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-# autoload -U promptinit; promptinit
-# source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-# [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+autoload -U promptinit; promptinit
+source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
-eval "$(starship init zsh)"
+# eval "$(starship init zsh)"
