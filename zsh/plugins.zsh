e167d0d1ae760c395611961757b0f664305703fb
Dusan Borovcanin <borovcanindusan1@gmail.com>
2023-09-13 17:25:46 +0200

    Use Vim in fif function

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -7,5 +7,6 @@ source <(kubectl completion zsh)
 # find-in-file - usage: fif <searchTerm>
 fif() {
   if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
-  rg --files-with-matches --no-messages "$1" | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' || rg --ignore-case --pretty --context 10 '$1' {}"
+  res=$(rg --files-with-matches --no-messages "$1" | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' || rg --ignore-case --pretty --context 10 '$1' {}")
+  if [[ ! -z "$res" ]]; then vim "$res"; return 0; fi
 }
