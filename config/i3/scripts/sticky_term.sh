adc93e2d04c69ed750ee0860e58e0f39e3e1b768
Dusan Borovcanin <borovcanindusan1@gmail.com>
2023-10-11 13:17:38 +0200

    Add sticky terminal script
    Signed-off-by: Dusan Borovcanin <borovcanindusan1@gmail.com>

---
+++ config/i3/scripts/sticky_term.sh
@@ -0,0 +1,26 @@
+#!/bin/sh	
+
+# Determine display size to set position and size of sticky terminal.
+res=$(xrandr | grep '*' | awk '{ print $1 }')
+
+
+st -n "sticky_term" &
+# i3-msg -q exec "st -n 'sticky_term'"
+
+pid="$!"
+
+# Wait for the window to open and grab its window ID
+sleep 0.3
+size="1200 700"
+position="715 55"
+echo $res
+
+case $res in
+  "1920x1080") position="1010 60" size="900 550" ;;
+  "1920x1200") position="715 55" size="1200 700" ;;
+  "2560x1440") position="1115 60" size="800 500" ;;
+  "3440x1440") position="1635 55" size="1800 1000" ;;
+esac
+
+i3-msg -q [instance="sticky_term"] resize set $size
+i3-msg -q [instance="sticky_term"] move position $position
